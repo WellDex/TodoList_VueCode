36fb1ef1a7a563484f23d9e2e0cc2d5eb17c6f23
Leo Masluyk <leonud2@mail.ru>
2020-05-28 00:11:55 +0300

    buil

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import 'material-design-icons-iconfont'
-
-Vue.config.productionTip = false
-
-new Vue({
-    render: h => h(App),
-}).$mount('#app')
